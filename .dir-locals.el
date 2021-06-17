75c0bdc05bf0ad24ca80b974aa8a0ddbc18fc41a
Karl Hallsby <karl@hallsby.com>
2021-06-17 17:12:27 -0500

    Add .dir-locals.el to override default C indentation style in emacs

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . ((c-default-style . "linux")
+	    ;(c-basic-offset . 2)
+	    (tab-width . 4)
+	    (indent-tabs-mode . nil))))
