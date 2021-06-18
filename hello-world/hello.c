a6799d3342fd2a441320219bc91a9ba83d78aac7
Karl Hallsby <karl@hallsby.com>
2021-06-18 09:00:39 -0500

    Add module documentation to prevent "tainted kernel" messages

--- hello-world/hello.c
+++ hello-world/hello.c
@@ -2,6 +2,9 @@
 #include <linux/kernel.h>
 
 #define MODULE_NAME "Hello_World"
+#define DRIVER_AUTHOR "Karl Hallsby <karl@hallsby.com>"
+#define DRIVER_DESCRIPTION "Small \"Hello World\" to practice kernel module \
+development."
 
 static int __init hello_init(void) {
         printk(KERN_INFO "Hello world!\n");
@@ -16,3 +19,6 @@ static void __exit hello_exit(void) {
 
 module_init(hello_init);
 module_exit(hello_exit);
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESCRIPTION);
+MODULE_LICENSE("GPL");
