db9f8991e20b8783550743f54a7423a49a265284
Karl Hallsby <karl@hallsby.com>
2021-06-28 11:39:41 -0500

    Introduce placeholder ioctl number for testing

--- fpga-char/chardev.h
+++ fpga-char/chardev.h
@@ -14,4 +14,8 @@
 int create_char_devs(struct fpga_device *fpga);
 int destroy_char_devs(void);
 
+/* The magic 'F' has MANY drivers. Some other sequence numbers (the second param)
+ * are taken. I use between 0x30 and 0x80 to give myself room to experiment. */
+#define FPGA_CHAR_DEADBEEF _IO('F', 0x30) // TODO: REMOVE THIS PLACEHOLDER NUM
+
 #endif
