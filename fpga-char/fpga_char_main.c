e4be39b7319efa276ea6fd0bced38a9d06583c13
Karl Hallsby <karl@hallsby.com>
2021-06-30 20:32:53 -0500

    Add goto handling/cleanup when init-ing FPGA module when probing pci

--- fpga-char/fpga_char_main.c
+++ fpga-char/fpga_char_main.c
@@ -42,9 +42,17 @@ static struct pci_driver fpga_driver = {
 static int fpga_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
         int error;
-        struct fpga_device *fpga;
         int bar;
         unsigned long dev_mmio_start, dev_mmio_len;
+
+        /* Allocate memory and initialize to zero for the driver's private
+         * data from the kernel's normal pool of memory.
+         * NOTE: The GFP_KERNEL flag means that the allocation is allowed to
+         * sleep. */
+        struct fpga_device *fpga = kzalloc(sizeof(struct fpga_device), GFP_KERNEL);
+        if(!fpga) { // error? NULL pointer returned
+                return -ENOMEM;
+        }
 
         /* We must enable the PCI device. This wakes the device up,
          * allocates I/O and memory regions.
@@ -53,14 +61,14 @@ static int fpga_probe(struct pci_dev *dev, const struct pci_device_id *id)
          * This is done by passing a bitmask of flags to a single backing
          * function. */
         error = pci_enable_device(dev);
-        if(error) {
-                // Could not enable the device. Exit.
-                return error;
+        if(error) { // error? non-zero returned
+                goto could_not_enable_device;
         }
 
         /* Create a bitmask of the BARs that match the provided flag configuration.
          * In this case, we are looking for BARs that are attached to memory. */
         bar = pci_select_bars(dev, IORESOURCE_MEM);
+        // TODO: If no BARs match, then 0 returned, not technically an error.
 
         /* Enable the PCI device, waking the device up, and and memory regions.
          * Lastly, enables the device's memory for use. If we wanted to use a BAR
@@ -69,35 +77,28 @@ static int fpga_probe(struct pci_dev *dev, const struct pci_device_id *id)
          * NOTE: fpga_enable_device() performs the same actions as
          * enable_device_(mem|io). */
         error = pci_enable_device_mem(dev);
-        if (error) {
-                return error;
+        if (error) { // error? non-zero returned
+                goto could_not_enable_device;
         }
 
         /* Request and attempt to reserve/lock the memory regions and/or IO that
          * the BARs of this device map to. */
         error = pci_request_region(dev, bar, DEVICE_NAME);
-        if (error) {
-                pci_disable_device(dev);
-                return error;
+        if (error) { // error? -EBUSY returned.
+                goto could_not_request_region;
         }
 
         /* Get start of BAR0 memory offset, and the length of BAR0. */
+        /* TODO: Should NOT do this if no BARs selected */
         dev_mmio_start = pci_resource_start(dev, 2);
-        dev_mmio_len = pci_resource_len(dev, 2);
-
-        /* Allocate memory and initialize to zero for the driver's private
-         * data from the kernel's normal pool of memory.
-         * NOTE: The GFP_KERNEL flag means that the allocation is allowed to
-         * sleep. */
-        fpga = kzalloc(sizeof(struct fpga_device), GFP_KERNEL);
-        if(!fpga) {
-                release_device(dev);
-                return -ENOMEM;
-        }
+        dev_mmio_len = pci_resource_len(dev, 2); // TODO: should be error if len == 0
 
         /* Bring the memory that the BAR points to into the CPU for use, and make
          * available as a pointer. */
         fpga->dev_mem = ioremap_uc(dev_mmio_start, dev_mmio_len);
+        if(!(fpga->dev_mem)) { // error? NULL pointer returned
+                goto ioremap_failed;
+        }
 
         // NOTE: Likely need to set up DMA. fpga_set_dma_mask()
 
@@ -108,13 +109,25 @@ static int fpga_probe(struct pci_dev *dev, const struct pci_device_id *id)
         printk(KERN_INFO "fpga_char: Vendor: 0x%X. Device: 0x%X\n",
                fpga->vendor_id, fpga->device_id);
 
-        create_char_devs(fpga);
+        error = create_char_devs(fpga);
+        if(error) { // error? non-zero returned
+                goto char_devs_failed;
+        }
 
         /* Defined in pci.h. Adds pointer to private struct to the DEVICE
          * struct that backs all other (sub)types device structs. */
         pci_set_drvdata(dev, fpga);
 
         return 0;
+
+char_devs_failed:
+        iounmap(fpga->dev_mem);
+ioremap_failed:
+        pci_release_region(dev, pci_select_bars(dev, IORESOURCE_MEM));
+could_not_request_region:
+        pci_disable_device(dev);
+could_not_enable_device:
+        return error;
 };
 
 /* This function is called whenever a PCIe device being handled by this driver
