8b0ef299eeeade979557858aa9d7531c56135739
Karl Hallsby <karl@hallsby.com>
2021-07-02 17:11:41 -0500

    Add shell.nix for compiling buildroot

---
+++ qemu-fpga-char/buildroot-shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> { }
+}:
+
+with pkgs;
+
+(pkgs.buildFHSUserEnv {
+  name = "buildroot-shell";
+  targetPkgs = pkgs: (with pkgs; [
+    binutils coreutils-full
+    gcc perl
+    python39 python39Packages.tkinter
+    wget curl
+    autoconf automake pkg-config
+    file which flock
+    rsync cpio
+    bc
+    readline62
+    ncurses libarchive openssl
+    unzip xz.dev lzma.dev bzip2.dev
+
+    breezy # Bazaar replacement
+    cvs git mercurial rsync subversion
+    jdk
+  ]);
+
+  # hardeningDisable = [ "all" ];
+
+}).env
