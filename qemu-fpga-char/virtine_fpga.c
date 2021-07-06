4d1b3188ce0ba135e03f4748ca28554bf3780663
Karl Hallsby <karl@hallsby.com>
2021-07-06 12:43:28 -0500

    Define MMIO register address offsets inside MMIO space

--- qemu-fpga-char/virtine_fpga.c
+++ qemu-fpga-char/virtine_fpga.c
@@ -35,6 +35,19 @@
  * |            Batch Factor           |
  * +-----------------------------------+ */
 
+#define MMIO_BASE_ADDR 0x0
+#define NUM_POSSIBLE_VIRTINES 100
+
+#define RQ_HEAD_OFFSET_REG MMIO_BASE_ADDR
+#define RQ_TAIL_OFFSET_REG RQ_HEAD_OFFSET_REG + sizeof(unsigned long)
+#define RQ_BASE_ADDR RQ_TAIL_OFFSET_REG + sizeof(unsigned long)
+#define DOORBELL_REG RQ_BASE_ADDR + (NUM_POSSIBLE_VIRTINES * sizeof(unsigned long))
+#define IS_PROCESSING_REG DOORBELL_REG + sizeof(unsigned long)
+#define CQ_HEAD_OFFSET_REG IS_PROCESSING_REG + sizeof(unsigned long)
+#define CQ_TAIL_OFFSET_REG CQ_HEAD_OFFSET_REG + sizeof(unsigned long)
+#define CQ_BASE_ADDR CQ_TAIL_OFFSET_REG + sizeof(unsigned long)
+#define BATCH_FACTOR_REG CQ_BASE_ADDR + (NUM_POSSIBLE_VIRTINES * sizeof(unsigned long))
+
 struct virtine_fpga_device {
         PCIDevice pdev;
         MemoryRegion mmio;
