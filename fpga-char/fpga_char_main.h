f9820e3f6d34160aa2e6c81bbd7cfcde2828f413
Karl Hallsby <karl@hallsby.com>
2021-07-13 00:15:36 -0500

    Include offsets in pci driver's header file

--- fpga-char/fpga_char_main.h
+++ fpga-char/fpga_char_main.h
@@ -16,4 +16,46 @@ struct fpga_device {
         u8 __iomem *dev_mem; // Pointer to mmap-ed device BAR in host's memory.
 };
 
+/* MMIO DESIGN (Remember PCI is little-endian):
+ * 0x0                               0x8
+ * +-----------------------------------+
+ * |    Ready Queue (RQ) Head offset   |
+ * +-----------------------------------+
+ * |    Ready Queue (RQ) Tail offset   |
+ * +-----------------------------------+
+ * |           RQ Virtine 1            |
+ * +-----------------------------------+
+ * |           RQ Virtine 2            |
+ * +-----------------------------------+
+ * |               .....               |
+ * +-----------------------------------+
+ * |              Doorbell             |
+ * +-----------------------------------+
+ * |          isCardProcessing         |
+ * +-----------------------------------+
+ * |  Complete Queue (CQ) Head offset  |
+ * +-----------------------------------+
+ * |  Complete Queue (CQ) Tail offset  |
+ * +-----------------------------------+
+ * |           CQ Virtine 1            |
+ * +-----------------------------------+
+ * |               .....               |
+ * +-----------------------------------+
+ * |            Batch Factor           |
+ * +-----------------------------------+ */
+
+#define MMIO_BASE_ADDR 0x0
+#define NUM_POSSIBLE_VIRTINES 100
+
+#define RQ_HEAD_OFFSET_REG MMIO_BASE_ADDR
+#define RQ_TAIL_OFFSET_REG RQ_HEAD_OFFSET_REG + sizeof(unsigned long)
+#define RQ_BASE_ADDR RQ_TAIL_OFFSET_REG + sizeof(unsigned long)
+#define DOORBELL_REG RQ_BASE_ADDR + (NUM_POSSIBLE_VIRTINES * sizeof(unsigned long))
+#define IS_PROCESSING_REG DOORBELL_REG + sizeof(unsigned long)
+#define CQ_HEAD_OFFSET_REG IS_PROCESSING_REG + sizeof(unsigned long)
+#define CQ_TAIL_OFFSET_REG CQ_HEAD_OFFSET_REG + sizeof(unsigned long)
+#define CQ_BASE_ADDR CQ_TAIL_OFFSET_REG + sizeof(unsigned long)
+#define BATCH_FACTOR_REG CQ_BASE_ADDR + (NUM_POSSIBLE_VIRTINES * sizeof(unsigned long))
+#define MAX_NUM_VIRTINES_REG BATCH_FACTOR_REG + (sizeof(unsigned long))
+
 #endif
