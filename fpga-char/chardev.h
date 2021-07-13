6b69731d5d876dff48a0ae19fb7d6f6507e49e2b
Karl Hallsby <karl@hallsby.com>
2021-07-13 00:18:12 -0500

    Fully document the ioctl macro creators in comment

--- fpga-char/chardev.h
+++ fpga-char/chardev.h
@@ -16,7 +16,17 @@ int create_char_devs(struct fpga_device *fpga);
 int destroy_char_devs(void);
 
 /* The magic 'F' has MANY drivers. Some other sequence numbers (the second param)
- * are taken. I use between 0x30 and 0x80 to give myself room to experiment. */
-#define FPGA_CHAR_FEEDBEAD _IO('F', 0x30) // TODO: REMOVE THIS PLACEHOLDER NUM
+ * are taken. I use between 0x30 and 0x80 to give myself room to experiment.
+ * To define a new ioctl number, I recommend you use one of the 4 macros below:
+ * _IO(magic, number) - No inputs/outputs
+ * _IOR(magic, number, input_data_type) - ioctl with input
+ * _IOW(magic, number, output_data_type) - ioctl with output
+ * _IORW(magic, number, in_out_data_type) - ioctl with input and output
+ * ALL ioctls THAT TAKE DATATYPE PARAMETERS ONLY TAKE THE PARAMETER!!
+ * i.e. _IOR(magic, number, struct struct_name), NOT
+ *      _IOR(magic, number, sizeof(struct struct_name)).
+ * Note that the struct is limited to a maximum of 16KiB (14 address bits) */
+#define IOCTL_MAGIC 'F'
+
 
 #endif
