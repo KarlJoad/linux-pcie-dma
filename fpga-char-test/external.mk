5db90a847ec7c28911aa15c65474c1e1b6b68e24
Karl Hallsby <karl@hallsby.com>
2021-07-13 11:11:39 -0500

    Update Makefile and external.mk to integrate test-ioctls to buildroot

--- fpga-char-test/external.mk
+++ fpga-char-test/external.mk
@@ -3,11 +3,12 @@ FPGA_CHAR_TEST_SITE = $(BR2_EXTERNAL_FPGA_CHAR_TEST_PATH)
 FPGA_CHAR_TEST_SITE_METHOD = local
 
 define FPGA_CHAR_TEST_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) test-addrs
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 define FPGA_CHAR_TEST_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/test-addrs $(TARGET_DIR)/usr/bin/test-addrs
+	$(INSTALL) -m 0755 -D $(@D)/test-ioctls $(TARGET_DIR)/usr/bin/test-ioctls
 endef
 
 $(eval $(generic-package))
