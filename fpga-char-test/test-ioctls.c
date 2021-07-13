a86d98c480c248a3a91022afd7d091691916c696
Karl Hallsby <karl@hallsby.com>
2021-07-13 11:32:35 -0500

    Check for ioctl failure after switch, simplifying each case

--- fpga-char-test/test-ioctls.c
+++ fpga-char-test/test-ioctls.c
@@ -54,9 +54,6 @@ int main(int argc, char **argv) {
     case MAX_VIRTINES: {
         unsigned long num_virtines = -1;
         ioctl_ret_val = ioctl(virtine_fd, FPGA_CHAR_GET_MAX_NUM_VIRTINES, &num_virtines);
-        if(ioctl_ret_val < 0 || num_virtines < 0) {
-            goto fail_exit;
-        }
         printf("Total supported virtines in each queue is %lu\n", num_virtines);
         break;
     }
@@ -68,15 +65,18 @@ int main(int argc, char **argv) {
         }
         unsigned long new_batch_factor = strtoul(argv[2], NULL, 0);
         ioctl_ret_val = ioctl(virtine_fd, FPGA_CHAR_MODIFY_BATCH_FACTOR, new_batch_factor);
-        if(ioctl_ret_val < 0) {
-            goto fail_exit;
+        if(ioctl_ret_val >= 0) {
+            printf("Changed Batch factor. Verify that the operation was successful.\n");
         }
-        printf("Changed Batch factor. Please verify that the operation was successful.\n");
         break;
     }
     default:
         printf("Unsupported ioctl test. Exiting!\n");
         return EXIT_FAILURE;
+    }
+
+    if(ioctl_ret_val < 0) {
+        goto fail_exit;
     }
 
     return EXIT_SUCCESS;
